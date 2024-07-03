212cb496d300edfae8351248d1cd8e1ac497c0d5
Ayan Maiti <mightyayan369@gmail.com>
2024-07-04 00:16:04 +0530

    Initial Commit: Implement Versioning and Assembly Referencing

---
+++ GenesisLibrary/StringCrypto.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace GenesisLibrary
+{
+    public class StringCrypto
+    {
+        public static string GetSHA256(string seed)
+        {
+            byte[] hashValue = SHA256.HashData(Encoding.UTF8.GetBytes(seed));
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < hashValue.Length; i++)
+            {
+                builder.Append(hashValue[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
+
+    }
+}
